36380b6f98e2d7f9455f7d3f88992e1a6b18eab9
mounika-2604 <mounikakolluru26@gmail.com>
2026-02-25 18:41:50 +0530

    Initial commit

---
+++ tdl.js
@@ -0,0 +1,38 @@
+const ib = document.getElementById("ib");
+const lc = document.getElementById("lc");
+
+function addtask(){
+    if(ib.value === ''){
+        alert("You have to write something...");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = ib.value;
+        lc.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // Unicode for × symbol
+        li.appendChild(span);
+    }
+    ib.value = "";
+    savedata();
+}
+
+lc.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("che");
+        savedata();
+    } else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+}, false);
+
+function savedata(){
+    localStorage.setItem("data", lc.innerHTML);
+}
+
+function showdata(){
+    lc.innerHTML = localStorage.getItem("data");
+}
+
+showdata();
